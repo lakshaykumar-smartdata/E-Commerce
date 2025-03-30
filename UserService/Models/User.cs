dfac17b8f70781cc8cd2ae40a8d195c6a1e00853
lakshaykumar-smartdata <lakshaykumar@smartdatainc.net>
2025-03-31 00:40:42 +0530

    #1

--- UserService/Models/User.cs
+++ UserService/Models/User.cs
@@ -2,6 +2,11 @@
 {
     public class User
     {
-
+        public int UserId { get; set; }
+        public int UserRoleId { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string ContactNumber { get; set; } = string.Empty;
     }
 }
