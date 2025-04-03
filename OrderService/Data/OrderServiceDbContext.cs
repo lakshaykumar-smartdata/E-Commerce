32e84f43b25472091ebcdaf5c08ca770f712c888
lakshaykumar-smartdata <lakshaykumar@smartdatainc.net>
2025-04-03 14:47:22 +0530

    "Added using statement for OrderService.Models and DbSet for Orders"

--- OrderService/Data/OrderServiceDbContext.cs
+++ OrderService/Data/OrderServiceDbContext.cs
@@ -1,11 +1,13 @@
 ﻿
 
 using Microsoft.EntityFrameworkCore;
+using OrderService.Models;
 
 namespace OrderService.Data
 {
     public class OrderServiceDbContext : DbContext
     {
         public OrderServiceDbContext(DbContextOptions<OrderServiceDbContext> options) : base(options) { }
+        public DbSet<Order> Orders { get; set; }
     }
 }
---
+++ OrderService/Models/Order.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OrderService.Models
+{
+    public class Order
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // Auto-increment for MSSQL
+        public int OrderId { get; set; }
+
+        [Required] // Ensures ProductId cannot be null
+        public Guid ProductId { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be at least 1")]
+        public int Quantity { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Status { get; set; } = "Processing";
+
+        [Required]
+        [Column(TypeName = "datetime2")]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
