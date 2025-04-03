fff5b1293de16f63218ee0c8521be71d13ad69d8
lakshaykumar-smartdata <lakshaykumar@smartdatainc.net>
2025-04-03 15:32:50 +0530

    `Updated API Gateway configuration, added new endpoints to OrderService, and modified Postman collection and OrderService project settings.`

--- OrderService/Services/IOrderService.cs
+++ OrderService/Services/IOrderService.cs
@@ -1,6 +1,14 @@
-﻿namespace OrderService.Services
+﻿
+using OrderService.Models;
+
+namespace OrderService.Services
 {
     public interface IOrderService
     {
+        Task<bool> DeleteOrderAsync(int orderId);
+        Task<IEnumerable<Order>> GetAllOrdersAsync();
+        Task<Order?> GetOrderByIdAsync(int orderId);
+        Task<Order> PlaceOrderAsync(Order order);
+        Task<bool> UpdateOrderStatusAsync(int orderId, string status);
     }
 }
